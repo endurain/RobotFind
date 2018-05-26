f70c98ffc142ba7d4a746c8c9ed39731341d55a1
endurain <zacharyjsanders@gmail.com>
2018-05-25 22:42:17 -0600

    reactapp

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Hunnikins',
+    username: 'KGRAY',
+    email: 'Sincere@april.biz'
+  },
+  {
+    id: 2,
+    name: 'Winnikins',
+    username: 'RoughRuff',
+    email: 'Shanna@melissa.tv'
+  },
+  {
+    id: 3,
+    name: 'Al Pal',
+    username: 'Pink Bubbles',
+    email: 'Nathan@yesenia.net'
+  },
+  {
+    id: 4,
+    name: 'Momma Gray',
+    username: 'Michigan4Life',
+    email: 'Julianne.OConner@kory.org'
+  },
+  {
+    id: 5,
+    name: 'Katie Wittenberg',
+    username: 'BakedGoods',
+    email: 'Lucio_Hettinger@annie.ca'
+  },
+  {
+    id: 6,
+    name: 'Gabe Rivero',
+    username: 'GMoney',
+    email: 'Karley_Dach@jasper.info'
+  },
+  {
+    id: 7,
+    name: 'Jim Sanders',
+    username: 'TotalNurd',
+    email: 'Telly.Hoeger@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'Dorthy Sanders',
+    username: 'MaGaMa',
+    email: 'Sherwood@rosamond.me'
+  },
+  {
+    id: 9,
+    name: 'Minnikins',
+    username: 'LilNugget',
+    email: 'Chaim_McDermott@dana.io'
+  },
+  {
+    id: 10,
+    name: 'Puppykins',
+    username: 'Omnomnom',
+    email: 'Rey.Padberg@karina.biz'
+  }
+];
